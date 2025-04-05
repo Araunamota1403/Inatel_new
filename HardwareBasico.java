8cccaa453bef100aab9821ea3930c55de0a7b1b0
Andreluiz <instandre14@gmail.com>
2025-04-04 22:26:00 -0300

    versão final projeto

--- HardwareBasico.java
+++
@@ -1,9 +0,0 @@
-public class HardwareBasico {
-    String nome;
-    int capacidade;
-
-    HardwareBasico(String nome, int capacidade) {
-        this.nome = nome;
-        this.capacidade = capacidade;
-    }
-}
